7c999cbe6a3b7481e5c96480b18c8af63f0111f1
surya-dev2006 <surya277728@gmail.com>
2026-05-05 14:40:44 +0530

    fixed sqlite deployment

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api"
+  baseURL: "https://myprojects-yt50.onrender.com/"
 });
 
 api.interceptors.request.use(cfg => {
